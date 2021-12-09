bdea44e03db13d1496c813b9716afc5faa4b89ae
DEBAPRAKASH <debaprakash.biswal31@gmail.com>
2021-12-09 08:55:32 +0200

    Logging

--- src/main/java/com/ioco/saviour/homosapienssurvival/controller/ReportController.java
+++ src/main/java/com/ioco/saviour/homosapienssurvival/controller/ReportController.java
@@ -5,6 +5,8 @@ import com.ioco.saviour.homosapienssurvival.dto.RobotDTO;
 import com.ioco.saviour.homosapienssurvival.service.RobotService;
 import com.ioco.saviour.homosapienssurvival.service.SurvivorService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +22,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/api/report")
+@Slf4j
 @Api(value = "HOMO SAPIENS SURVIVAL REPORT API")
 public class ReportController {
 
@@ -29,15 +32,27 @@ public class ReportController {
     @Autowired
     private SurvivorService survivorService;
 
+    @ApiOperation(
+            value = "get list of Robots",
+            code = 200,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            response = RobotDTO.class)
     @GetMapping(value="/fetchRobotList", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RobotDTO>> getRobotDetails(){
         List<RobotDTO> robotDTOS = robotService.fetchRobotDetails();
+        log.info("Robots List size:"+ robotDTOS.size());
         return new ResponseEntity<>(robotDTOS, HttpStatus.OK);
     }
 
+    @ApiOperation(
+            value = "get list of Robots based on RobotType",
+            code = 200,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            response = RobotDTO.class)
     @GetMapping(value="/fetchRobotListBasedOnType", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RobotDTO>> getRobotDetailsPerType(@RequestParam RobotType robotType){
         List<RobotDTO> robotDTOS = robotService.fetchRobotDetails(robotType.name());
+        log.info("Robots List size: for "+ robotType + "-" +robotDTOS.size());
         return new ResponseEntity<>(robotDTOS,HttpStatus.OK);
     }
 
--- src/main/java/com/ioco/saviour/homosapienssurvival/controller/SurvivorController.java
+++ src/main/java/com/ioco/saviour/homosapienssurvival/controller/SurvivorController.java
@@ -1,11 +1,14 @@
 package com.ioco.saviour.homosapienssurvival.controller;
 
 import com.ioco.saviour.homosapienssurvival.dto.LocationDTO;
+import com.ioco.saviour.homosapienssurvival.dto.RobotDTO;
 import com.ioco.saviour.homosapienssurvival.dto.SurvivorDTO;
 import com.ioco.saviour.homosapienssurvival.entity.Survivor;
 import com.ioco.saviour.homosapienssurvival.service.SurvivorService;
 import com.ioco.saviour.homosapienssurvival.util.SurvivorUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,39 +23,64 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/survivor")
 @Api(value = "HOMO SAPIENS SURVIVAL API")
+@Slf4j
 public class SurvivorController {
 
     @Autowired
     private SurvivorService survivorService;
 
+    @ApiOperation(
+            value = "Create Survivors",
+            code = 200,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            response = Survivor.class)
     @PostMapping("/create")
     ResponseEntity<Survivor> createSurvivor(@Valid @RequestBody SurvivorDTO survivorDTO) {
         Survivor survivor = SurvivorUtil.populateSurvivor(survivorDTO);
         survivorService.createSurvivor(survivor);
+        log.info("Survivor Created Id:"+ survivor.getId());
         return new ResponseEntity(survivor, HttpStatus.CREATED);
     }
 
+    @ApiOperation(
+            value = "Update locations for Survivors",
+            code = 200)
     @PostMapping("/updateLocation")
     public void updateSurvivor(@Valid @RequestBody LocationDTO locationDTO) {
         survivorService.updateSurvivorLocation(locationDTO);
+        log.info("Survivor Location Updated Id:"+ locationDTO.getSurvivorId());
     }
 
+    @ApiOperation(
+            value = "Flag Infected Survivors",
+            code = 200)
     @PostMapping("/flagInfected")
     public void flagInfection(@RequestBody long survivorId) {
         survivorService.flagInfection(survivorId);
+        log.info("Survivor infected flagged Id:"+ survivorId);
     }
 
-    //List of infected vs non infected
+    @ApiOperation(
+            value = "get list of Infected Survivors",
+            code = 200,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            response = Survivor.class)
     @GetMapping(value="/fetchInfectedSurvivors", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Survivor>> getInfectedSurvivors(){
         List<Survivor> infectedSurvivors = survivorService.findAllInfectedSurvivor();
+        log.info("Infected Survivour List size:"+ infectedSurvivors.size());
         return new ResponseEntity<>(infectedSurvivors,HttpStatus.OK);
     }
 
+    @ApiOperation(
+            value = "get list of NonInfected Survivors",
+            code = 200,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            response = Survivor.class)
     @GetMapping(value="/fetchNonInfectedSurvivors", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Survivor>> getNonInfectedSurvivors(){
         List<Survivor> nonInfectedSurvivors = survivorService.findAllNonInfectedSurvivor();
+        log.info("Non Infected Survivour List size:"+ nonInfectedSurvivors.size());
         return new ResponseEntity<>(nonInfectedSurvivors,HttpStatus.OK);
     }
-
 }
